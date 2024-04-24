d69e52140bcd2e21962ba769261af7223e5748eb
Andrew Meads <andrew.meads@auckland.ac.nz>
2024-04-24 16:27:06 +1200

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.css";
+
+export default function App() {
+
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>Hello, WDCC! 🐮💻</h1>
+      </div>
+    </>
+  );
+}
