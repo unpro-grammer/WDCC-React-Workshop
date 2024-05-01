76baafaed0de09e79f411c5495a61f949e63705e
Andrew Meads <andrew.meads@auckland.ac.nz>
2024-05-01 14:26:03 +1200

    Updated README with Gist link, removed some files which are now gists.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # WDCC React Workshop - Starter project
+
+In addition to the source code in this starter project, we'll also be using [gists](https://docs.github.com/en/get-started/writing-on-github/editing-and-sharing-content-with-gists/creating-gists) to share code during the session.
+
+## Gist link
+
+The secret gist link for this lab is:
+
+- <https://gist.github.com/andrew-meads/4610bcfa9e6addc619522eadfdfda07f>
---
+++ frontend/src/data/initial-contacts.js
@@ -0,0 +1,51 @@
+export const INITIAL_CONTACTS = [
+  {
+    _id: "055f1aaa-aab0-4ff3-a709-85c28f1d397f",
+    name: "Luke Skywalker",
+    phoneNumber: "+1 555-000-1001",
+    funFact: "Has a green lightsaber in 'Return of the Jedi'.",
+    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+  },
+  {
+    _id: "5f90d372-7bdf-4b39-b621-cf6d3cd975f4",
+    name: "Hermione Granger",
+    phoneNumber: "+1 555-000-1002",
+    funFact: "Uses the Time-Turner in 'The Prisoner of Azkaban'.",
+    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+  },
+  {
+    _id: "b8853e3f-38d1-4712-9071-8f62d71c19b8",
+    name: "Walter White",
+    phoneNumber: "+1 555-000-1003",
+    funFact: "Is also known as Heisenberg.",
+    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+  },
+  {
+    _id: "18fb9676-3c5b-4480-b8bc-4b9cc2f6ef28",
+    name: "Darth Vader",
+    phoneNumber: "+1 555-000-1004",
+    funFact: "Was originally a Jedi named Anakin Skywalker.",
+    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+  },
+  {
+    _id: "983c620f-6500-476f-a856-8791aac32794",
+    name: "Tony Stark",
+    phoneNumber: "+1 555-000-1005",
+    funFact: "Is the real name of Iron Man.",
+    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+  },
+  {
+    _id: "99fb1c56-98b3-41a9-b67b-580567495e86",
+    name: "Sherlock Holmes",
+    phoneNumber: "+1 555-000-1006",
+    funFact: "Plays the violin when he is thinking.",
+    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+  },
+  {
+    _id: "71b5ed07-9e1e-47d0-937a-64c396d5567b",
+    name: "Bruce Wayne",
+    phoneNumber: "+1 555-000-1007",
+    funFact: "Lives in Gotham City.",
+    photoUrl: "https://th.bing.com/th/id/OIP.PoS7waY4-VeqgNuBSxVUogAAAA?rs=1&pid=ImgDetMain"
+  }
+];
