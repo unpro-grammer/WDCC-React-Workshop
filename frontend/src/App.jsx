4f36e051bcfd5176e403faf1bf58420dbc4a74e6
Grace Xu <gxu660@aucklanduni.ac.nz>
2024-05-04 14:44:10 +1200

    Allow navigating between contct lists

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,22 @@ import styles from "./App.module.css";
 import ContactDisplay from "./components/ContactDisplay";
 import { INITIAL_CONTACTS } from "./data/initial-contacts";
 import Sidebar from "./components/Sidebar";
+import { useState } from "react";
 
 export default function App() {
 
-  const contact = {
-    _id: "18fb9676-3c5b-4480-b8bc-4b9cc2f6ef28",
-    name: "Darth Vader",
-    phoneNumber: "+1 555-000-1004",
-    funFact: "Was originally a Jedi named Anakin Skywalker.",
-    photoUrl: "https://preview.redd.it/standing-cat-pfp-pls-v0-ovdg2gh57p4c1.jpg?auto=webp&s=98703885e0eced7caea79ebabaf0c7c7559680a2"
-  }
+  const [contact, setContact] = useState(INITIAL_CONTACTS[0]); //create stateful value called contact
 
-  function handleContactClick(contact) {
-    console.log(contact);
+  // let contact = {
+  //   _id: "18fb9676-3c5b-4480-b8bc-4b9cc2f6ef28",
+  //   name: "Darth Vader",
+  //   phoneNumber: "+1 555-000-1004",
+  //   funFact: "Was originally a Jedi named Anakin Skywalker.",
+  //   photoUrl: "https://preview.redd.it/standing-cat-pfp-pls-v0-ovdg2gh57p4c1.jpg?auto=webp&s=98703885e0eced7caea79ebabaf0c7c7559680a2"
+  // }
+
+  function handleContactClick(c) {
+    setContact(c);
   }
 
   return (
